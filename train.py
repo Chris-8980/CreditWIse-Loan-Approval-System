7680633315e965db3aa98fbe06ad82cf2cd67c22
Chris <chrisjhamodia@gmail.com>
2026-08-26 11:52:19 +0530

    Complete Day 2 EDA and feature preprocessing

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer    
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 df = pd.read_csv("loan_approval_data.csv")
 # print(df.head())
 # print(df.info())
@@ -22,3 +23,30 @@ df[categorical_cols] = cat_imp.fit_transform(df[categorical_cols])
 # print(df.isnull().sum()) 
 
 # print(df.head())
+
+
+# Remove Applicant Id
+df = df.drop("Applicant_ID", axis=1)
+
+# Encoding
+le = LabelEncoder()
+
+df["Education_Level"] = le.fit_transform(df["Education_Level"])
+df["Loan_Approved"] = le.fit_transform(df["Loan_Approved"])
+
+
+cols = ["Employment_Status", "Marital_Status", "Loan_Purpose", "Property_Area", "Gender", "Employer_Category",]
+
+ohe = OneHotEncoder(drop="first", sparse_output=False, handle_unknown="ignore")
+encoded = ohe.fit_transform(df[cols])
+encoded_df = pd.DataFrame(encoded, columns=ohe.get_feature_names_out(cols), index=df.index)
+df = pd.concat([df.drop(columns=cols), encoded_df], axis=1)
+
+
+# Correlation Heatmap
+num_cols = df.select_dtypes(include="number")
+corr_matrix = num_cols.corr()
+
+plt.figure(figsize=(15, 8))
+sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm")
+plt.show()
