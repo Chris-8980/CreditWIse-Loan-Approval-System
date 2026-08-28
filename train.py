7980ceb4b77ba6799c015a6090228d59cba09e4b
Chris <chrisjhamodia@gmail.com>
2026-08-28 10:19:19 +0530

    Complete Day 3 model training and evaluation along with Feature Engineering

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer    
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 df = pd.read_csv("loan_approval_data.csv")
 # print(df.head())
 # print(df.info())
@@ -49,4 +51,131 @@ corr_matrix = num_cols.corr()
 
 plt.figure(figsize=(15, 8))
 sns.heatmap(corr_matrix, annot=True, fmt=".2f", cmap="coolwarm")
-plt.show()
+# plt.show()
+
+# Train-test split and Feature Scaling
+X = df.drop("Loan_Approved", axis=1)
+y = df["Loan_Approved"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Training a Logistic Regression model
+from sklearn.linear_model import LogisticRegression
+log_model = LogisticRegression()
+log_model.fit(X_train_scaled, y_train)
+
+log_pred = log_model.predict(X_test_scaled)
+
+# Evaluate
+print("Logistic Regression Evaluation Metrics:")
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score, confusion_matrix
+print("Precision:", precision_score(y_test, log_pred))
+print("Accuracy:", accuracy_score(y_test, log_pred))   
+print("Recall:", recall_score(y_test, log_pred))
+print("F1 Score:", f1_score(y_test, log_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, log_pred))
+print("\n\n")
+
+# Training a KNN model
+from sklearn.neighbors import KNeighborsClassifier
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train_scaled, y_train)
+knn_pred = knn_model.predict(X_test_scaled)
+
+# Evaluate
+print("\nKNN Evaluation Metrics:")
+print("Precision:", precision_score(y_test, knn_pred))
+print("Accuracy:", accuracy_score(y_test, knn_pred))
+print("Recall:", recall_score(y_test, knn_pred))
+print("F1 Score:", f1_score(y_test, knn_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, knn_pred))
+print("\n\n")
+
+# Training a Naive Bayes model
+from sklearn.naive_bayes import GaussianNB
+nb_model = GaussianNB()
+nb_model.fit(X_train_scaled, y_train)
+nb_pred = nb_model.predict(X_test_scaled)
+
+# Evaluate
+print("\nNaive Bayes Evaluation Metrics:")
+print("Precision:", precision_score(y_test, nb_pred))
+print("Accuracy:", accuracy_score(y_test, nb_pred))
+print("Recall:", recall_score(y_test, nb_pred))
+print("F1 Score:", f1_score(y_test, nb_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, nb_pred))
+print("\n\n")
+
+# Best Model On the Basis of Precision => Naive Bayes
+
+# Add or Tranform features
+df["DTI_Ratio_sq"] = df["DTI_Ratio"] ** 2
+df["Credit_Score_sq"] = df["Credit_Score"] ** 2
+
+# df["Applicant_Income_log"] = np.log1p(df["Applicant_Income"])
+
+X = df.drop(columns=["Loan_Approved", "Credit_Score", "DTI_Ratio"])
+y = df["Loan_Approved"]
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scaling
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Logistic regression
+
+log_model = LogisticRegression()
+log_model.fit(X_train_scaled, y_train)
+
+y_pred = log_model.predict(X_test_scaled)
+
+# Evaluation
+print("Logistic Regression Model")
+print("Precision: ", precision_score(y_test, y_pred))
+print("Recall: ", recall_score(y_test, y_pred))
+print("F1 score: ", f1_score(y_test, y_pred))
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+print("CM: ", confusion_matrix(y_test, y_pred))
+print("\n\n")
+
+# KNN
+
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train_scaled, y_train)
+
+y_pred = knn_model.predict(X_test_scaled)
+
+# Evaluation
+print("KNN Model")
+print("Precision: ", precision_score(y_test, y_pred))
+print("Recall: ", recall_score(y_test, y_pred))
+print("F1 score: ", f1_score(y_test, y_pred))
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+print("CM: ", confusion_matrix(y_test, y_pred))
+print("\n\n")
+
+# Naive Bayes
+
+nb_model = GaussianNB()
+nb_model.fit(X_train_scaled, y_train)
+
+y_pred = nb_model.predict(X_test_scaled)
+
+# Evaluation
+print("Naive Bayes Model")
+print("Precision: ", precision_score(y_test, y_pred))
+print("Recall: ", recall_score(y_test, y_pred))
+print("F1 score: ", f1_score(y_test, y_pred))
+print("Accuracy: ", accuracy_score(y_test, y_pred))
+print("CM: ", confusion_matrix(y_test, y_pred))
+
+# Best Overall Model => Logistic Regression with Feature Engineering
+# Best Model Based on Precision => Naive Bayes
